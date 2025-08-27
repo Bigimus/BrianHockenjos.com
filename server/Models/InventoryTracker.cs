27e9b4210e7c0062cd8e7fe62c5bcf50f75223bc
Brian Hockenjos <67165421+Bigimus@users.noreply.github.com>
2025-08-26 22:01:53 -0400

    updates

--- server/Models/InventoryTracker.cs
+++
@@ -1,9 +0,0 @@
-namespace server.Models{
-    public class InventoryTracker {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public int Remaining { get; set; } = 0;
-        public int Total { get; set; } = 0;
-        public required string  Link { get; set; }
-    }
-}
